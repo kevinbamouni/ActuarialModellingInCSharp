b3a6f0d5d9b758ad6c0306a410d6db27df996c77
Kevin BAMOUNI <kevincedric.y@gmail.com>
2021-12-22 02:23:51 +0100

    Amend au précédent commit

---
+++ BasicTermS/DataFromCsv.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+
+namespace BasicTermS
+{
+    public static class DataFromCsv
+    {
+
+        public static Type GetColumnType(string type)
+        {
+            var result = typeof(string);
+
+            switch (type)
+            {
+                case "int":
+                    result = typeof(int);
+                    break;
+                case "long":
+                    result = typeof(long);
+                    break;
+                case "float":
+                    result = typeof(float);
+                    break;
+                case "double":
+                    result = typeof(double);
+                    break;
+                case "decimal":
+                    result = typeof(decimal);
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        //
+        // Params :
+        //
+        // Dictionary<string, string> paramColumnDictionnary = {"ColumnName1":"ColumnType1",
+        //                                                      "ColumnName2":"ColumnType2",
+        //                                                      "ColumnName3":"ColumnType3",
+        //                                                      ...}
+        //
+        public static DataTable AddColumnWithType(DataTable paramDataTable, Dictionary<string, string> paramColumnDictionnary)
+        {
+            foreach(var item in paramColumnDictionnary)
+            {
+                paramDataTable.Columns.Add(item.Key, GetColumnType(item.Value));
+            }
+            return paramDataTable;
+        }
+
+    }
+}
+
---
+++ BasicTermS/Hypotheses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace BasicTermS
+{
+    public class Hypotheses
+    {
+        public DataTable MortTable;
+        public string PathMortTable;
+
+        public Hypotheses(string pathMortTable)
+        {
+            PathMortTable = pathMortTable;
+
+            MortTable = new DataTable();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                NewLine = Environment.NewLine,
+                Delimiter = ";",
+                HasHeaderRecord = true,
+            };
+
+            using (var reader = new StreamReader(pathMortTable))
+            using (var csv = new CsvReader(reader, config))
+            {
+                // Do any configuration to `CsvReader` before creating CsvDataReader.
+                using (var dr = new CsvDataReader(csv))
+                {
+                    MortTable.Load(dr);
+                }
+            }
+        }
+    }
+}
+
