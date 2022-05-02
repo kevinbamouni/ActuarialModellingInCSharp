e49d69d6e0bab8c04433dfc876ed1c8b3cec736e
Kevin BAMOUNI <kevincedric.y@gmail.com>
2022-05-02 09:02:33 +0200

    Mise à jour des Inputs et des paramètres de lecture des Inputs.

--- SimpleLife/Input.cs
+++ SimpleLife/Input.cs
@@ -7,29 +7,90 @@ using System.Threading.Tasks;
 namespace SimpleLife
 {
     public static class Input
-    {
-        public static string PathAssumptions = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\disc_rate_ann.csv";
-        public static string SchemasAssumptions = @"{""year"":""int"",""zero_spot"":""decimal""}";
-
-        public static string PathParamAssumptions = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\disc_rate_ann.csv";
-        public static string SchemasParamAssumptions = @"{""year"":""int"",""zero_spot"":""decimal""}";
-
-        public static string SchemasMortRate = @"{""MortTableID"":""int"",""Age"":""int"",""Sex"":""string"",""Qx"":""decimal""}";
+    {   /////////////////////////////////////////////////////////////ok
+        public static string PathAssumptions = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\mort_lapse_assumption.csv";
+        public static string SchemasAssumptions = @"{""Year"":""int"",
+                                                        ""MortAsmp1"":""decimal"",
+                                                        ""MortAsmp2"":""decimal"",
+                                                        ""Morb1"":""decimal"",
+                                                        ""Morb2"":""decimal"",
+                                                        ""Morb3"":""decimal"",
+                                                        ""Morb4"":""decimal"",
+                                                        ""Morb5"":""decimal"",
+                                                        ""LapseRate1"":""decimal""}";
+        /////////////////////////////////////////////////////////////ok
+        public static string PathParamAssumptions = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\assumption.csv";
+        public static string SchemasParamAssumptions = @"{""Product"": ""string"",
+                                                            ""PolType"": ""string"",
+                                                            ""Gen"": ""int"",
+                                                            ""BaseMort"": ""int"",
+                                                            ""MortFactor"": ""string"",
+                                                            ""Surrender"": ""string"",
+                                                            ""Renewal"": ""decimal"",
+                                                            ""InvstRet"": ""decimal"",
+                                                            ""CommInitPrem"": ""decimal"",
+                                                            ""CommRenPrem"": ""decimal"",
+                                                            ""CommRenTerm"": ""decimal"",
+                                                            ""ExpsAcqSA"": ""decimal"",
+                                                            ""ExpsAcqAnnPrem"": ""decimal"",
+                                                            ""ExpsAcqPol"": ""decimal"",
+                                                            ""ExpsMaintSA"": ""decimal"",
+                                                            ""ExpsMaintPrem"": ""decimal"",
+                                                            ""ExpsMaintPol"": ""decimal"",
+                                                            ""InflRate"": ""decimal"",
+                                                            ""CnsmpTax"": ""decimal""}";
+        /////////////////////////////////////////////////////////////ok
         public static string PathMortRate = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\mortality_table.csv";
-
-        public static string SchemasLapse = @"{""Age"":""int"",""0"":""decimal"",""1"":""decimal"",""2"":""decimal"",""3"":""decimal"",""4"":""decimal"",""5"":""decimal""}";
-        public static string PathLapse = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\mortality_table.csv";
-
-        public static string SchemasProductSpec = @"{""Age"":""int"",""0"":""decimal"",""1"":""decimal"",""2"":""decimal"",""3"":""decimal"",""4"":""decimal"",""5"":""decimal""}";
-        public static string PathProductSpec = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\mortality_table.csv";
-
-        public static string SchemasScenarios = @"{""Age"":""int"",""0"":""decimal"",""1"":""decimal"",""2"":""decimal"",""3"":""decimal"",""4"":""decimal"",""5"":""decimal""}";
-        public static string PathScenarios = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\mortality_table.csv";
-
+        public static string SchemasMortRate = @"{""MortTableID"":""int"",
+                                                    ""Age"":""int"",
+                                                    ""Sex"":""string"",
+                                                    ""Qx"":""decimal""}";
+        /////////////////////////////////////////////////////////////ok
+        public static string PathProductSpec = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\productspec.csv";
+        public static string SchemasProductSpec = @"{""Product"": ""string"",
+                                                    ""PolType"": ""string"",
+                                                    ""Gen"": ""string"",
+                                                    ""LoadAcqSAParam1"": ""decimal"",
+                                                    ""LoadAcqSAParam2"": ""decimal"",
+                                                    ""LoadMaintSA"": ""decimal"",
+                                                    ""LoadMaintSA2"": ""decimal"",
+                                                    ""LoadMaintPremParam1"": ""decimal"",
+                                                    ""LoadMaintPremParam2"": ""decimal"",
+                                                    ""SurrChargeParam1"": ""decimal"",
+                                                    ""SurrChargeParam2"": ""decimal"",
+                                                    ""MortTablePrem"": ""decimal"",
+                                                    ""MortTableVal"": ""decimal"",
+                                                    ""IntRatePrem"": ""decimal"",
+                                                    ""IntRateVal"": ""decimal"",
+                                                    ""LapseRatePrem"": ""decimal"",
+                                                    ""LapseRateVal"": ""decimal"",
+                                                    ""Hsx"": ""decimal"",
+                                                    ""Tsx"": ""decimal"",
+                                                    ""Hax"": ""decimal"",
+                                                    ""Tax"": ""decimal""}";
+        /////////////////////////////////////////////////////////////ok
+        public static string PathScenarios = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\scenarios.csv";
+        public static string SchemasScenarios = @"{""ScenID"":""int"",
+                                                    ""Year"":""int"",
+                                                    ""IntRate"":""decimal""}";
+        /////////////////////////////////////////////////////////////ok
         public static string PathModelPoint = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\policydata.csv";
-        public static string SchemasModelPoint = @"{""point_id"":""int"",""age_at_entry"":""int"",""sex"":""string"",""policy_term"":""int"",""policy_count"":""decimal"",""sum_assured"":""decimal""}";
-
-        public static string PathModelPointTest = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\model_point_table_test.csv";
+        public static string SchemasModelPoint = @"{""Policy"":""int"",
+                                                    ""Product"":""string"",
+                                                    ""PolicyType"":""int"",
+                                                    ""Gen"":""int"",
+                                                    ""Channel"":""int"",
+                                                    ""Duration"":""int"",
+                                                    ""Sex"":""string"",
+                                                    ""IssueAge"":""int"",
+                                                    ""PaymentMode"":""int"",
+                                                    ""PremFreq"":""int"",
+                                                    ""PolicyTerm"":""int"",
+                                                    ""MaxPolicyTerm"":""int"",
+                                                    ""PolicyCount"":""int"",
+                                                    ""SumAssured"":""int""}";
+        /// //////////////////////////////////////////////////////////ok
+        public static string PathModelPointTest = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\policydata_test.csv";
 
         public static string results = @"C:\Users\work\source\repos\ActuarialModellingInCSharp\SimpleLife\Data\results.json";
     }
